644d8bde0aba84576591309900aa70327b14881a
Rahulkumar <rahulkumar109422@gmail.com>
2023-03-07 16:01:50 +0530

    ci: create an util function to fetch weather data

---
+++ src/utils/FetchWeather.jsx
@@ -0,0 +1,12 @@
+export default async function fetchWeather(lat, long) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${
+    import.meta.env.VITE_WEATHER_API_KEY
+  }`;
+
+  const res = await fetch(url, {
+    cache: "no-cache",
+    referrerPolicy: "no-referrer"
+  });
+
+  return await res.json();
+}
