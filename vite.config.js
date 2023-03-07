f82ad1c71538b78292e5a160894b669e3696a406
Rahulkumar <rahulkumar109422@gmail.com>
2023-03-07 08:23:54 +0530

    initialize vite configuration file

---
+++ vite.config.js
@@ -0,0 +1,32 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import legacy from "@vitejs/plugin-legacy";
+import lightningcss from "vite-plugin-lightningcss";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    legacy({
+      targets: ["defaults", "not IE 11"]
+    }),
+    lightningcss({
+      browserslist: [">0.25%", "not dead", "not op_mini all", "not IE 11"],
+      drafts: {
+        nesting: true
+      },
+      pseudoClasses: {
+        focusVisible: "focus-visible"
+      }
+    })
+  ],
+
+  server: {
+    open: true,
+    port: 5000
+  },
+
+  preview: {
+    open: true,
+    port: 5050
+  }
+});
