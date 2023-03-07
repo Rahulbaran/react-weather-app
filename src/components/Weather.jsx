0e62900e48549e2e2a4fade0f0fc00886c99e0f0
Rahulkumar <rahulkumar109422@gmail.com>
2023-03-07 16:02:45 +0530

    ci: create weather component with some boilerplate code

---
+++ src/components/Weather.jsx
@@ -0,0 +1,9 @@
+export default function Weather({ weatherData }) {
+  console.table(weatherData);
+
+  return (
+    <main className="container weather-container">
+      <h1>WELCOME TO WEATHER CONTAINER</h1>
+    </main>
+  );
+}
