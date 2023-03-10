a47779e291bf7e27d4a08b3dfce9c9f2ecbf582d
Rahulkumar <rahulkumar109422@gmail.com>
2023-03-10 17:54:12 +0530

    ci: import util functions and Components & use it

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+/* Components */
+import Weather from "./components/Weather";
+
+/* utils */
+import fetchWeather from "./utils/FetchWeather";
 
 export default function App() {
+  const [loc, setLoc] = useState({
+    lat: 45.34,
+    long: 45.43
+  });
+  const [weatherData, setWeatherData] = useState("");
+
+  useEffect(() => {
+    fetchWeather(loc.lat, loc.long)
+      .then(res => setWeatherData(res))
+      .catch(rej => console.error(rej));
+  }, [loc]);
+
+  const getPosition = () => {
+    const success = pos =>
+      setLoc({ lat: pos.coords.latitude, long: pos.coords.longitude });
+    const error = err => {
+      throw Error(err);
+    };
+
+    navigator.geolocation.getCurrentPosition(success, error, {
+      enableHighAccuracy: true,
+      timeout: 15000
+    });
+  };
+
   return (
     <React.Fragment>
-      <h1>Good Morning Weather App</h1>
+      {weatherData && <Weather getPosition={getPosition} w={weatherData} />}
     </React.Fragment>
   );
 }
