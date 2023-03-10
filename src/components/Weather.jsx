9f626e1de83d278838c554a89df99a4fdfbc7468
Rahulkumar <rahulkumar109422@gmail.com>
2023-03-10 17:52:09 +0530

    ci: include required content to Weather Card component

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,9 +1,39 @@
-export default function Weather({ weatherData }) {
-  console.table(weatherData);
+import { getLocalDate, getLocalTime } from "../utils/TimeModifier";
 
+export default function Weather({ w, getPosition }) {
   return (
     <main className="container weather-container">
-      <h1>WELCOME TO WEATHER CONTAINER</h1>
+      <div className="card weather-card">
+        <div className="card-header">
+          <h1>{w.name}</h1>
+          <button>Refresh</button>
+        </div>
+
+        <div className="card-body">
+          <div className="time-info-wrapper">
+            <h2>{getLocalDate(new Date())}</h2>
+            <h2>{w.weather[0].description}</h2>
+          </div>
+
+          <div className="temp-info-wrapper">
+            <p>
+              Temperature: {w.main.temp} <sup>o</sup>C
+            </p>
+            <p>Humidity: {w.main.humidity} %</p>
+          </div>
+
+          <div className="sun-info-wrapper">
+            <p>Sunrise: {getLocalTime(w.sys.sunrise)}</p>
+            <p>Sunset: {getLocalTime(w.sys.sunset)}</p>
+          </div>
+        </div>
+
+        <div className="card-footer">
+          <button className="btn primary-btn" onClick={getPosition}>
+            Get Info
+          </button>
+        </div>
+      </div>
     </main>
   );
 }
