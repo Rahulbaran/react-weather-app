7f2dccf7bb846247529df4276b983a39ed1fb7ab
Rahulkumar <rahulkumar109422@gmail.com>
2023-03-10 17:38:33 +0530

    ci: include new query string in weather API URL
    - Query String for "unit" was included

--- src/utils/FetchWeather.jsx
+++ src/utils/FetchWeather.jsx
@@ -1,7 +1,7 @@
 export default async function fetchWeather(lat, long) {
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${
     import.meta.env.VITE_WEATHER_API_KEY
-  }`;
+  }&units=metric`;
 
   const res = await fetch(url, {
     cache: "no-cache",
